d837bf3b48acb06cf8decd854da74b15908e3512
Mirsait <cabmupcaut@gmail.com>
2026-03-03 19:01:10 +0300

    Create Task1.java

---
+++ HW1/Task1.java
@@ -0,0 +1,10 @@
+public static int evklid(int a, int b){
+        while(a != 0 && b != 0){
+            if(a > b){
+                a = a % b;
+            }else{
+                b = b % a;
+            }
+        }
+        return a + b;
+    }
