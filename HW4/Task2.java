db4f7754521d9142dd40bfae91105c0583bb2be6
Mirsait <cabmupcaut@gmail.com>
2026-03-17 13:50:18 +0300

    Update Task2.java

--- HW4/Task2.java
+++ HW4/Task2.java
@@ -1,1 +1,16 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println(findDeletedNumber(new int[]{1, 2, 3, 4, 5, 6,7, 8, 9, 10, 11, 13}));
+    }
 
+    public static int findDeletedNumber(int[] arr){
+        int res = 0;
+        for(int i = 0; i <= arr[arr.length - 1]; i++){
+            res += i;
+        }
+        for(int i = 0; i < arr.length; i++){
+            res -= arr[i];
+        }
+        return res;
+    }
+}
