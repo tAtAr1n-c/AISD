2f54fc8e86f2ac20c9e58d7218f7db869fa953e4
Mirsait <cabmupcaut@gmail.com>
2026-03-19 22:26:22 +0300

    Create Task1.java

---
+++ HW4/Task1.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+    }
+
+  
+    public static boolean findShift(String s1, String s2){
+        return (s1 + s1).contains(s2);
+    }
+
+}
