b3c92d7914bf96feb00015d717d1adf9ea07f312
Mirsait <cabmupcaut@gmail.com>
2026-03-29 20:10:00 +0300

    Update FibonacciHeap.java

--- Semestrovka1/FibonacciHeap.java
+++ Semestrovka1/FibonacciHeap.java
@@ -4,44 +4,62 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
-public class FibonacciHeap<T> {
+public class FibonacciHeap {
 
-
-    private static final class Node<T> {
-        private int priority;
-        private final T value;
+    private static final class Node {
+        private int key;
         private int degree;
         private boolean mark;
-        private Node<T> parent;
-        private Node<T> child;
-        private Node<T> left;
-        private Node<T> right;
+        private Node parent;
+        private Node child;
+        private Node left;
+        private Node right;
 
-        private Node(int priority, T value) {
-            this.priority = priority;
-            this.value = value;
+        private Node(int key) {
+            this.key = key;
             this.left = this;
             this.right = this;
         }
     }
 
-
-
-    private Node<T> min;
+    private Node min;
     private int size;
 
-
-
-
-    // 1) Добавление
-    public void add(int priority, T value) {
-        Node<T> node = new Node<>(priority, value);
+    public void add(int key) {
+        Node node = new Node(key);
         addToRootList(node);
         size++;
     }
-    private void addToRootList(Node<T> node) {
+
+    public boolean isEmpty() {
+        return min == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Integer extractMin() {
+        Node node = extractMinInternal();
+        return node == null ? null : node.key;
+    }
+
+    public boolean search(int key) {
+        return searchInList(min, key) != null;
+    }
+
+    public boolean delete(int key) {
+        Node node = searchInList(min, key);
+        if (node == null) {
+            return false;
+        }
+        decreaseKey(node, Integer.MIN_VALUE);
+        extractMinInternal();
+        return true;
+    }
+
+    private void addToRootList(Node node) {
         if (min == null) {
             node.left = node;
             node.right = node;
@@ -54,28 +72,22 @@ public class FibonacciHeap<T> {
         min.right.left = node;
         min.right = node;
 
-        if (node.priority < min.priority) {
+        if (node.key < min.key) {
             min = node;
         }
     }
 
-
-    // 2) Поиск
-    public boolean search(T value) {
-        return searchInList(min, value) != null;
-    }
-
-    private Node<T> searchInList(Node<T> start, T value) {
+    private Node searchInList(Node start, int key) {
         if (start == null) {
             return null;
         }
 
-        Node<T> current = start;
+        Node current = start;
         do {
-            if (Objects.equals(current.value, value)) {
+            if (current.key == key) {
                 return current;
             }
-            Node<T> fromChild = searchInList(current.child, value);
+            Node fromChild = searchInList(current.child, key);
             if (fromChild != null) {
                 return fromChild;
             }
@@ -85,36 +97,25 @@ public class FibonacciHeap<T> {
         return null;
     }
 
-
-
-    // 3) Удаление
-    public boolean delete(T value) {
-        Node<T> node = searchInList(min, value);
-        if (node == null) {
-            return false;
-        }
-        decreasePriority(node, Integer.MIN_VALUE);
-        extractMinInternal();
-        return true;
-    }
-    private void decreasePriority(Node<T> node, int newPriority) {
-        if (newPriority > node.priority) {
-            throw new IllegalArgumentException("New priority is greater than current priority");
+    private void decreaseKey(Node node, int newKey) {
+        if (newKey > node.key) {
+            throw new IllegalArgumentException("New key is greater than current key");
         }
 
-        node.priority = newPriority;
-        Node<T> parent = node.parent;
+        node.key = newKey;
+        Node parent = node.parent;
 
-        if (parent != null && node.priority < parent.priority) {
+        if (parent != null && node.key < parent.key) {
             cut(node, parent);
             cascadingCut(parent);
         }
 
-        if (min == null || node.priority < min.priority) {
+        if (min == null || node.key < min.key) {
             min = node;
         }
     }
-    private void cut(Node<T> node, Node<T> parent) {
+
+    private void cut(Node node, Node parent) {
         if (parent.child == node) {
             parent.child = (node.right == node) ? null : node.right;
         }
@@ -128,12 +129,9 @@ public class FibonacciHeap<T> {
         node.right = node;
         addToRootList(node);
     }
-    private void removeFromList(Node<T> node) {
-        node.left.right = node.right;
-        node.right.left = node.left;
-    }
-    private void cascadingCut(Node<T> node) {
-        Node<T> parent = node.parent;
+
+    private void cascadingCut(Node node) {
+        Node parent = node.parent;
         if (parent == null) {
             return;
         }
@@ -145,15 +143,16 @@ public class FibonacciHeap<T> {
             cascadingCut(parent);
         }
     }
-    private Node<T> extractMinInternal() {
-        Node<T> z = min;
+
+    private Node extractMinInternal() {
+        Node z = min;
         if (z == null) {
             return null;
         }
 
         if (z.child != null) {
-            List<Node<T>> children = collectSiblings(z.child);
-            for (Node<T> child : children) {
+            List<Node> children = collectSiblings(z.child);
+            for (Node child : children) {
                 child.parent = null;
                 child.left = child;
                 child.right = child;
@@ -166,7 +165,7 @@ public class FibonacciHeap<T> {
         if (z == z.right) {
             min = null;
         } else {
-            Node<T> next = z.right;
+            Node next = z.right;
             removeFromList(z);
             min = next;
             consolidate();
@@ -180,21 +179,20 @@ public class FibonacciHeap<T> {
         return z;
     }
 
-
     private void consolidate() {
-        Map<Integer, Node<T>> degreeTable = new HashMap<>();
-        List<Node<T>> roots = collectSiblings(min);
+        Map<Integer, Node> degreeTable = new HashMap<>();
+        List<Node> roots = collectSiblings(min);
 
-        for (Node<T> root : roots) {
-            Node<T> x = root;
+        for (Node root : roots) {
+            Node x = root;
             x.left = x;
             x.right = x;
             int degree = x.degree;
 
             while (degreeTable.containsKey(degree)) {
-                Node<T> y = degreeTable.remove(degree);
-                if (x.priority > y.priority) {
-                    Node<T> tmp = x;
+                Node y = degreeTable.remove(degree);
+                if (x.key > y.key) {
+                    Node tmp = x;
                     x = y;
                     y = tmp;
                 }
@@ -205,7 +203,7 @@ public class FibonacciHeap<T> {
         }
 
         min = null;
-        for (Node<T> node : degreeTable.values()) {
+        for (Node node : degreeTable.values()) {
             node.left = node;
             node.right = node;
             if (min == null) {
@@ -216,7 +214,7 @@ public class FibonacciHeap<T> {
         }
     }
 
-    private void link(Node<T> childNode, Node<T> parentNode) {
+    private void link(Node childNode, Node parentNode) {
         removeFromList(childNode);
         childNode.parent = parentNode;
         childNode.mark = false;
@@ -226,7 +224,7 @@ public class FibonacciHeap<T> {
             childNode.left = childNode;
             childNode.right = childNode;
         } else {
-            Node<T> child = parentNode.child;
+            Node child = parentNode.child;
             childNode.left = child;
             childNode.right = child.right;
             child.right.left = childNode;
@@ -236,13 +234,18 @@ public class FibonacciHeap<T> {
         parentNode.degree++;
     }
 
-    private List<Node<T>> collectSiblings(Node<T> start) {
-        List<Node<T>> nodes = new ArrayList<>();
+    private void removeFromList(Node node) {
+        node.left.right = node.right;
+        node.right.left = node.left;
+    }
+
+    private List<Node> collectSiblings(Node start) {
+        List<Node> nodes = new ArrayList<>();
         if (start == null) {
             return nodes;
         }
 
-        Node<T> current = start;
+        Node current = start;
         do {
             nodes.add(current);
             current = current.right;
@@ -250,11 +253,5 @@ public class FibonacciHeap<T> {
 
         return nodes;
     }
-
-
-
-
-
-
-
 }
+
